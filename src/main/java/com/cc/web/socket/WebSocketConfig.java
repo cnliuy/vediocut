103e71a11cc9782cddba3e429c3e4d7695484497
cnliuy <cnliuy@163.com>
2016-09-18 18:27:55 +0800

    添加 websocket

---
+++ src/main/java/com/cc/web/socket/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.cc.web.socket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app"); //目的地的 前缀
+	}
+
+	//@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/receivemsg").withSockJS();
+	}
+
+}
---
+++ src/main/java/com/cc/web/socket/controller/MsgController.java
@@ -0,0 +1,29 @@
+package com.cc.web.socket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.WebSocketSession;
+
+
+
+@Controller
+public class MsgController {
+	
+    @MessageMapping("/receivemsg")
+    @SendTo("/topic/servicemsg")
+    public String receiveMsg(String message) throws Exception {
+        //Thread.sleep(3000); //simulated delay
+        System.out.println("in  MsgController() --- Msg method, message:"+message);
+        return message;
+    }
+    
+    
+    @MessageMapping("/signstat")
+    @SendTo("/topic/servicemsg")
+    //@SendTo("/topic/sign")    , WebSocketSession session ,
+    public String signStat( WebSocketSession session , String message) throws Exception {    		
+        System.out.println("in  MsgController() --- signStat, sessionId: message:"+message);
+        return message;
+    }
+}
